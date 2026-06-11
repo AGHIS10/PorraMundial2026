768a05d076909ed6e1df359922332f9128aadd81
Hisagus <ochenta.chisteras0e@icloud.com>
2026-06-11 18:37:23 +0200

    Varios jugadores

--- docs/clasificacion.js
+++ docs/clasificacion.js
@@ -7,8 +7,44 @@ window.__CLASIFICACION__ = [
   },
   {
     "posicion": 2,
-    "nombre": "Mario",
-    "aciertos": 43,
-    "puntos": 43
+    "nombre": "GEMINI",
+    "aciertos": 19,
+    "puntos": 19
+  },
+  {
+    "posicion": 3,
+    "nombre": "GPT",
+    "aciertos": 18,
+    "puntos": 18
+  },
+  {
+    "posicion": 4,
+    "nombre": "SERGIO",
+    "aciertos": 18,
+    "puntos": 18
+  },
+  {
+    "posicion": 5,
+    "nombre": "PEDRO",
+    "aciertos": 17,
+    "puntos": 17
+  },
+  {
+    "posicion": 6,
+    "nombre": "CRISTIAN",
+    "aciertos": 16,
+    "puntos": 16
+  },
+  {
+    "posicion": 7,
+    "nombre": "MARIO",
+    "aciertos": 14,
+    "puntos": 14
+  },
+  {
+    "posicion": 8,
+    "nombre": "GABRIEL",
+    "aciertos": 12,
+    "puntos": 12
   }
 ];
